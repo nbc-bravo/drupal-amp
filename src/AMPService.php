0ff4a4bee18e8d80018240c1b166b247305eebeb
Sidharth Kshatriya <sid.kshatriya@gmail.com>
2016-01-28 17:37:15 +0530

    Make a service for the amp library

---
+++ src/AMPService.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\amp\AMPService.
+ */
+
+namespace Drupal\amp;
+
+
+/**
+ * Class AMPService.
+ *
+ * @package Drupal\amp
+ */
+class AMPService  {
+  /**
+   * Constructor.
+   */
+  public function __construct() {
+
+  }
+
+}
