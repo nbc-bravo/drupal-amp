8cab5da2b0206085812d128cccf63c77aee7697c
mdrummond <mdrummond@mac.com>
2016-01-29 16:24:45 -0600

    Add form item for choosing the AMP theme.

--- src/Form/AmpSettingsForm.php
+++ src/Form/AmpSettingsForm.php
@@ -7,14 +7,32 @@
 
 namespace Drupal\amp\Form;
 
+use Drupal\Core\Config\ConfigFactoryInterface;
+use Drupal\Core\Extension\ThemeHandlerInterface;
 use Drupal\Core\Form\ConfigFormBase;
 use Drupal\Core\Form\FormStateInterface;
 use Drupal\node\Entity\NodeType;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
  * Defines the configuration export form.
  */
 class AmpSettingsForm extends ConfigFormBase {
+
+  /**
+   * The theme handler service.
+   *
+   * @var \Drupal\Core\Extension\ThemeHandlerInterface
+   */
+  protected $themeHandler;
+
+  /**
+   * The array of valid theme options.
+   *
+   * @array $themeOptions
+   */
+  private $themeOptions;
+
 
   /**
    * {@inheritdoc}
@@ -27,23 +45,77 @@ class AmpSettingsForm extends ConfigFormBase {
    * {@inheritdoc}
    */
   protected function getEditableConfigNames() {
-    return ['amp.settings'];
+    return ['amp.settings', 'amp.theme'];
+  }
+
+  /*
+   * Helper function to get available theme options.
+   *
+   * @return array
+   *   Array of valid themes.
+   */
+  private function getThemeOptions() {
+    // Get all available themes.
+    $themes = $this->themeHandler->rebuildThemeData();
+    uasort($themes, 'system_sort_modules_by_info_name');
+    $theme_options = [];
+
+    foreach ($themes as $theme) {
+      if (!empty($theme->info['hidden']) && !empty($theme->status) && $theme->status == 'installed') {
+        $theme_options[$theme->getName()] = $theme->info['name'];
+      }
+    }
+
+    return $theme_options;
+  }
+
+  /**
+   * Constructs a AmpSettingsForm object.
+   *
+   * @param \Drupal\Core\Config\ConfigFactoryInterface $config_factory
+   *   The factory for configuration objects.
+   * @param \Drupal\Core\Extension\ThemeHandlerInterface $theme_handler
+   *   The theme handler.
+   */
+  public function __construct(ConfigFactoryInterface $config_factory, ThemeHandlerInterface $theme_handler) {
+    parent::__construct($config_factory);
+
+    $this->themeHandler = $theme_handler;
+    $this->themeOptions = $this->getThemeOptions();
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container) {
+    return new static(
+      $container->get('config.factory'),
+      $container->get('theme_handler')
+    );
   }
 
   /**
    * {@inheritdoc}
    */
   public function buildForm(array $form, FormStateInterface $form_state) {
-    $config = $this->config('amp.settings');
-
+    $nodetype_config = $this->config('amp.settings');
     $node_types = node_type_get_names();
-
     $form['node_types'] = array(
       '#type' => 'checkboxes',
       '#multiple' => TRUE,
       '#title' => $this->t('Select nodes that have AMP versions by default:'),
-      '#default_value' => $config->get('node_types'),
+      '#default_value' => $nodetype_config->get('node_types'),
       '#options' => $node_types,
+    );
+
+    $amp_theme_options = $this->themeOptions;
+    $amptheme_config = $this->config('amp.theme');
+    $form['amptheme'] = array(
+      '#type' => 'select',
+      '#options' => $amp_theme_options,
+      '#title' => $this->t('AMP theme'),
+      '#description' => $this->t('Choose a theme to use for AMP pages.'),
+      '#default_value' => $amptheme_config->get('amptheme'),
     );
 
     return parent::buildForm($form, $form_state);
@@ -54,11 +126,16 @@ class AmpSettingsForm extends ConfigFormBase {
    */
   public function submitForm(array &$form, FormStateInterface $form_state) {
 
-    if ($form_state->hasValue('node_types')) {
+    if ($form_state->hasValue('node_types') && $form_state->hasValue('amptheme')) {
       $node_types = $form_state->getValue('node_types');
-      $config = $this->config('amp.settings');
-      $config->setData(['node_types' => $node_types]);
-      $config->save();
+      $nodetype_config = $this->config('amp.settings');
+      $nodetype_config->setData(['node_types' => $node_types]);
+      $nodetype_config->save();
+
+      $amptheme = $form_state->getValue('amptheme');
+      $amptheme_config = $this->config('amp.theme');
+      $amptheme_config->setData(['amptheme' => $amptheme]);
+      $amptheme_config->save();
 
       parent::submitForm($form, $form_state);
     }
