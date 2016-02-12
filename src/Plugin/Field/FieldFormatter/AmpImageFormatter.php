8ef16daa7854d08fa1883ed521c51a87949d2c21
Matthew Tift <mctift@gmail.com>
2016-02-12 12:36:15 -0600

    #22 Display amp-image settings

--- src/Plugin/Field/FieldFormatter/AmpImageFormatter.php
+++ src/Plugin/Field/FieldFormatter/AmpImageFormatter.php
@@ -39,14 +39,6 @@ class AmpImageFormatter extends ImageFormatter {
   public function settingsForm(array $form, FormStateInterface $form_state) {
     $element = parent::settingsForm($form, $form_state);
 
-    $layouts = array(
-      'nodisplay',
-      'fixed',
-      'responsive',
-      'fixed-height',
-      'fill',
-      'container',
-    );
     $layout_url = 'https://www.ampproject.org/docs/guides/responsive/control_layout.html#size-and-position-elements';
     // Add configuration options for layout.
     $element['amp_layout'] = [
@@ -54,10 +46,40 @@ class AmpImageFormatter extends ImageFormatter {
       '#type' => 'select',
       '#default_value' => $this->getSetting('amp_layout'),
       '#empty_option' => t('None (no layout)'),
-      '#options' => $layouts,
+      '#options' => $this->getLayouts(),
       '#description' => $this->t('<a href=":url" target="_blank">Layout Information</a>', array(':url' => $layout_url)),
     ];
 
     return $element;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function settingsSummary() {
+    $summary = parent::settingsSummary();
+
+    // Display this setting only if an AMP layout is set.
+    $layout_options = $this->getLayouts();
+    $layout_setting = $this->getSetting('amp_layout');
+    if (isset($layout_options[$layout_setting])) {
+      $summary[] = t('Layout: @setting', array('@setting' => $layout_options[$layout_setting]));
+    }
+
+    return $summary;
+  }
+
+  /**
+   * Return a list of AMP layouts.
+   */
+  private function getLayouts() {
+    return [
+      'nodisplay' => 'nodisplay',
+      'fixed' => 'fixed',
+      'responsive' => 'responsive',
+      'fixed-height' => 'fixed-height',
+      'fill' => 'fill',
+      'container' => 'container',
+    ];
   }
 }
