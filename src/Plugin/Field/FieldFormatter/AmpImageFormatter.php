1ff9752d01919cd4eefa219f27e8728f102493aa
Matthew Tift <mctift@gmail.com>
2016-02-12 12:19:53 -0600

    Add configuration option for layouts to amp-img

--- src/Plugin/Field/FieldFormatter/AmpImageFormatter.php
+++ src/Plugin/Field/FieldFormatter/AmpImageFormatter.php
@@ -8,6 +8,7 @@
 namespace Drupal\amp\Plugin\Field\FieldFormatter;
 
 use Drupal\Core\Field\FieldItemListInterface;
+use Drupal\Core\Form\FormStateInterface;
 use Drupal\image\Plugin\Field\FieldFormatter\ImageFormatter;
 
 /**
@@ -30,5 +31,33 @@ class AmpImageFormatter extends ImageFormatter {
   public function viewElements(FieldItemListInterface $items, $langcode) {
     $elements = parent::viewElements($items);
     return $elements;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function settingsForm(array $form, FormStateInterface $form_state) {
+    $element = parent::settingsForm($form, $form_state);
+
+    $layouts = array(
+      'nodisplay',
+      'fixed',
+      'responsive',
+      'fixed-height',
+      'fill',
+      'container',
+    );
+    $layout_url = 'https://www.ampproject.org/docs/guides/responsive/control_layout.html#size-and-position-elements';
+    // Add configuration options for layout.
+    $element['amp_layout'] = [
+      '#title' => t('AMP Layout'),
+      '#type' => 'select',
+      '#default_value' => $this->getSetting('amp_layout'),
+      '#empty_option' => t('None (no layout)'),
+      '#options' => $layouts,
+      '#description' => $this->t('<a href=":url" target="_blank">Layout Information</a>', array(':url' => $layout_url)),
+    ];
+
+    return $element;
   }
 }
