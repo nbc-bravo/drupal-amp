82c1e62152ed68b28c9b47d85c8f8ae1d22ee044
Matthew Tift <mctift@gmail.com>
2016-02-16 12:18:15 -0600

    #12 Add stub code for AMP adsense block

---
+++ src/Plugin/Block/AmpGoogleAdsenseBlock.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Provides an AMP Google Adsense block
+ *
+ * @Block(
+ *   id = "amp_google_adsense_block",
+ *   admin_label = @Translation("AMP Google Adsense block"),
+ * )
+ */
+
+namespace Drupal\amp\Plugin\Block;
+
+use Drupal\Core\Block\BlockBase;
+
+class AmpGoogleAdsenseBlock extends BlockBase {
+  /**
+   * {@inheritdoc}
+   */
+  public function build() {
+    $amp_config = \Drupal::config('amp.settings');
+    if (empty($amp_config->get('google_adsense_id'))) {
+      return array(
+        '#markup' => $this->t('This block requires a Google Adsense ID.')
+      );
+    }
+    return array(
+      '#markup' => $this->t('It works.')
+    );
+  }
+}
