c43453da636046961184728f34e168078ca2d250
Matthew Tift <mctift@gmail.com>
2016-02-16 10:15:30 -0600

    #31 Add Iframe field formatter that uses the AMP library

---
+++ src/Plugin/Field/FieldFormatter/AmpIframeFormatter.php
@@ -0,0 +1,48 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\amp\Plugin\Field\FieldFormatter\AmpIframeFormatter.
+ */
+
+namespace Drupal\amp\Plugin\Field\FieldFormatter;
+
+use Drupal\text\Plugin\Field\FieldFormatter\TextDefaultFormatter;
+use Drupal\Core\Field\FieldItemListInterface;
+use Lullabot\AMP\AMP;
+use Drupal;
+
+/**
+ * Plugin implementation of the 'amp_iframe' formatter.
+ *
+ * @FieldFormatter(
+ *   id = "amp_iframe",
+ *   label = @Translation("AMP Iframe"),
+ *   description = @Translation("Display amp-iframe content."),
+ *   field_types = {
+ *     "string",
+ *     "text",
+ *     "text_long",
+ *     "text_with_summary",
+ *   },
+ * )
+ */
+class AmpIframeFormatter extends TextDefaultFormatter {
+  /**
+   * {@inheritdoc}
+   */
+  public function viewElements(FieldItemListInterface $items, $langcode) {
+    $amp_service = Drupal::getContainer()->get('amp.utilities');
+    $amp = $amp_service->getAMPConverter();
+    $elements = parent::viewElements($items, $langcode);
+    foreach ($elements as &$element) {
+      $amp->loadHtml($element['#text']);
+      $element['#text'] = $amp->convertToAmpHtml();
+    }
+    $amp->clear();
+    return $elements;
+  }
+
+}
+
+
