c0167d2e66d87d389075e07fb15098b48bf36f54
Matthew Tift <mctift@gmail.com>
2016-02-18 16:26:37 -0600

    Only access amp settings config once

--- src/Plugin/Block/AmpGoogleAdsenseBlock.php
+++ src/Plugin/Block/AmpGoogleAdsenseBlock.php
@@ -23,11 +23,12 @@ class AmpGoogleAdsenseBlock extends BlockBase {
         '#markup' => $this->t('This block requires a Google Adsense ID.')
       );
     }
-    $adsense_id = \Drupal::config('amp.settings')->get('adsense_id');
-    $adsense_width = \Drupal::config('amp.settings')->get('google_adsense_width');
-    $adsense_height = \Drupal::config('amp.settings')->get('google_adsense_height');
-    $adsense_dataadclient = \Drupal::config('amp.settings')->get('google_adsense_dataadclient');
-    $adsense_dataadslot = \Drupal::config('amp.settings')->get('google_adsense_dataadslot');
+    $amp_settings = \Drupal::config('amp.settings');
+    $adsense_id = $amp_settings->get('adsense_id');
+    $adsense_width = $amp_settings->get('google_adsense_width');
+    $adsense_height = $amp_settings->get('google_adsense_height');
+    $adsense_dataadclient = $amp_settings->get('google_adsense_dataadclient');
+    $adsense_dataadslot = $amp_settings->get('google_adsense_dataadslot');
     return array(
       '#markup' => $this->t('<!-- google_adsense_id is @adsense -->', array('@adsense' => $adsense_id))
     );
