ca484efcf6c9df790c815d42f37d4f045b32a80b
mdrummond <mdrummond@mac.com>
2016-02-19 11:08:14 -0600

    Add width and height settings on field formatter.

--- src/Plugin/Field/FieldFormatter/AmpVideoFormatter.php
+++ src/Plugin/Field/FieldFormatter/AmpVideoFormatter.php
@@ -8,6 +8,7 @@
 namespace Drupal\amp\Plugin\Field\FieldFormatter;
 
 use Drupal\Core\Field\FieldItemListInterface;
+use Drupal\Core\Form\FormStateInterface;
 use Drupal\file\Plugin\Field\FieldFormatter\GenericFileFormatter;
 
 /**
@@ -32,8 +33,62 @@ class AmpVideoFormatter extends GenericFileFormatter {
 
     foreach ($elements as $delta => $element) {
       $elements[$delta]['#theme'] = 'amp_video';
+      $elements[$delta]['#attributes']['height'] = $this->getSetting('height');
+      $elements[$delta]['#attributes']['width'] = $this->getSetting('width');
     }
 
     return $elements;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function defaultSettings() {
+    return array(
+      'height' => 175,
+      'width' => 350,
+    ) + parent::defaultSettings();
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function settingsForm(array $form, FormStateInterface $form_state) {
+    $element = parent::settingsForm($form, $form_state);
+
+    $element['height'] = array(
+      '#type' => 'number',
+      '#title' => t('Height'),
+      '#size' => 10,
+      '#default_value' => $this->getSetting('height'),
+    );
+
+    $element['width'] = array(
+      '#type' => 'number',
+      '#title' => t('Width'),
+      '#size' => 10,
+      '#default_value' => $this->getSetting('width'),
+    );
+
+    return $element;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function settingsSummary() {
+    $summary = parent::settingsSummary();
+
+    $height_setting = $this->getSetting('height');
+    if (isset($height_setting)) {
+      $summary[] = t('Height: @height' . 'px', array('@height' => $height_setting));
+    }
+
+    $width_setting = $this->getSetting('width');
+    if (isset($width_setting)) {
+      $summary[] = t('Width: @width' . 'px', array('@width' => $width_setting));
+    }
+
+    return $summary;
   }
 }
