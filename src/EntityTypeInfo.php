f82a50f0c48c37e9c27a610f712a5a232611fcaf
Matthew Tift <mctift@gmail.com>
2016-03-10 20:58:24 -0600

    Fix AMP status when module is first installed

--- src/EntityTypeInfo.php
+++ src/EntityTypeInfo.php
@@ -19,17 +19,17 @@ class EntityTypeInfo {
    *   An array of bundles that have AMP view modes enabled.
    */
   public function getAmpEnabledTypes() {
+    $enabled_types = [];
     if ($cache = \Drupal::cache()->get('amp_enabled_types')) {
       $enabled_types = $cache->data;
     }
     else {
       $node_types = array_keys(node_type_get_names());
       foreach ($node_types as $node_type) {
-        $amp_display = entity_get_display('node', $node_type, 'amp');
-        //$amp_display = \Drupal::entityManager()
-          //->getStorage('entity_view_display')
-          //->load('node.' . $node_type . '.amp');
-        if ($amp_display->status()) {
+        $amp_display = \Drupal::entityManager()
+          ->getStorage('entity_view_display')
+          ->load('node.' . $node_type . '.amp');
+        if ($amp_display && $amp_display->status()) {
           $enabled_types[] = $node_type;
         }
       }
