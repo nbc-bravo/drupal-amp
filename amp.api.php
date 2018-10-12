4e4e5111f670bceb0a360c7ae619e325d988ad92
quicksketch <nate@quicksketch.org>
2018-10-12 10:23:33 -0700

    Issue #3006316: Update AMP Library list.

--- amp.api.php
+++ amp.api.php
@@ -43,3 +43,13 @@ function hook_amp_metadata_alter(&$metadata_json, $node, $type) {
 function hook_amp_converter_alter($amp) {
 
 }
+
+/**
+ * Modify the list of AMP JS components.
+ *
+ * @param array $js_list
+ *
+ */
+function hook_amp_js_list_alter($js_list) {
+  $js_list['amp-some-component'] = 'https://cdn.ampproject.org/v0/amp-some-component-0.1.js';
+}
