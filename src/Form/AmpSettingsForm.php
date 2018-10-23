920408145ec08a04c39a38e0e7473c7e2c37f6a0
karens <karens@45874.no-reply.drupal.org>
2018-10-23 07:53:41 -0500

    Issue #3008569 by KarenS: Move AMP Pixel to separate module

--- src/Form/AmpSettingsForm.php
+++ src/Form/AmpSettingsForm.php
@@ -259,44 +259,6 @@ class AmpSettingsForm extends ConfigFormBase {
       '#placeholder' => 'UA-',
       '#access' => FALSE,
     ];
-    $form['pixel_group'] = array(
-      '#type' => 'fieldset',
-      '#title' => t('amp-pixel'),
-      '#access' => FALSE,
-    );
-    $form['pixel_group']['amp_pixel'] = array(
-      '#type' => 'checkbox',
-      '#title' => $this->t('Enable amp-pixel'),
-      '#default_value' => $amp_config->get('amp_pixel'),
-      '#description' => $this->t('The amp-pixel element is meant to be used as a typical tracking pixel -- to count page views. Find more information in the <a href="https://www.ampproject.org/docs/reference/amp-pixel.html">amp-pixel documentation</a>.'),
-    );
-    $form['pixel_group']['amp_pixel_domain_name'] = array(
-      '#type' => 'textfield',
-      '#title' => $this->t('amp-pixel domain name'),
-      '#default_value' => $amp_config->get('amp_pixel_domain_name'),
-      '#description' => $this->t('The domain name where the tracking pixel will be loaded: do not include http or https.'),
-      '#states' => array('visible' => array(
-        ':input[name="amp_pixel"]' => array('checked' => TRUE))
-      ),
-    );
-    $form['pixel_group']['amp_pixel_query_string'] = array(
-      '#type' => 'textfield',
-      '#title' => $this->t('amp-pixel query path'),
-      '#default_value' => $amp_config->get('amp_pixel_query_string'),
-      '#description' => $this->t('The path at the domain where the GET request will be received, e.g. "pixel" in example.com/pixel?RANDOM.'),
-      '#states' => array('visible' => array(
-        ':input[name="amp_pixel"]' => array('checked' => TRUE))
-      ),
-    );
-    $form['pixel_group']['amp_pixel_random_number'] = array(
-      '#type' => 'checkbox',
-      '#title' => $this->t('Random number'),
-      '#default_value' => $amp_config->get('amp_pixel_random_number'),
-      '#description' => $this->t('Use the special string RANDOM to add a random number to the URL if required. Find more information in the <a href="https://github.com/ampproject/amphtml/blob/master/spec/amp-var-substitutions.md#random">amp-pixel documentation</a>.'),
-      '#states' => array('visible' => array(
-        ':input[name="amp_pixel"]' => array('checked' => TRUE))
-      ),
-    );
 
     // @TODO Display again once this is possible.
     // The configuration option still exists so it can be used in AMP logic.
